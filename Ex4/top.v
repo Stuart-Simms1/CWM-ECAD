096ae1032f35a37c588862fc295a96ad06aa9d40
Stuart-Simms1 <stuart.simms@stcatz.ox.ac.uk>
2021-06-15 13:06:23 +0000

    Exercise 4 1st attempt, I think any errors are coming from timing issues

--- Ex4/top.v
+++ Ex4/top.v
@@ -16,3 +16,38 @@
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+module lights(
+	input clk,
+	input rst,
+	input button,
+	output reg [2:0]colour);
+	
+	
+	initial begin	//to define colour from the beginning (not necessary as we reset it in the simulation immediately)
+	colour = 3'b001;
+	end
+	
+	always @(posedge clk) begin
+		if(rst) begin		//resetting takes precedence
+			colour <= 3'b001;
+		end
+		else begin	
+			if((colour == 3'b000)||(colour == 3'b111)) begin	//dealing with the colours out of scope
+				colour <= 3'b001;					
+			end
+			else begin
+				if (button) begin
+					case(colour)
+						3'b110: colour <= 3'b001;				//110 + 1 would take us out of scope so set it to 001
+						default: colour <= colour + 3'b1;		//works for all the other colours
+					endcase
+				end
+				else begin
+					colour <= colour;							//keep it the same for no button push
+				end
+			end
+		end
+	end
+	
+endmodule
--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -7,3 +7,99 @@
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+module top_tb();
+
+	//parameters
+	parameter CLK_PERIOD = 10;
+	
+	//regs and wires
+	reg clk;
+	reg rst;
+	reg button;
+	reg err;
+	wire [2:0] colour;
+	reg [2:0] prev;
+	
+	//clock
+	initial begin	//this sets the clock going from the start
+   		clk = 1'b0;
+    	forever begin
+    	   #(CLK_PERIOD/2);
+    	   clk=~clk;
+    	end
+    end
+	//logic
+	initial begin	//sending the data stream
+		rst = 1;
+		button = 0;
+		#CLK_PERIOD;
+		forever begin
+			rst = 0;
+			#CLK_PERIOD;
+			button = 1;
+			#(10*CLK_PERIOD);
+			button = 0;
+			#(2*CLK_PERIOD);
+			rst = 1;
+			#CLK_PERIOD;
+		end	
+	end
+	
+	initial begin		//this will detect the errors
+		err = 0;
+		forever begin
+			prev = colour;
+			#CLK_PERIOD;
+			err = 0;
+			if(rst) begin
+				if(colour != 001)begin
+					err = 1;
+					$display("Error with reset");
+				end
+			end
+			else begin
+				if(colour == 3'b000 || colour == 3'b111) begin
+					err = 1;
+					$display("Error with unused colour");
+				end
+				if(~button) begin
+					if (colour != prev) begin
+						$display("Error with button off");
+						err = 1;
+					end
+				end
+				else begin
+					if (colour != (prev + 3'b1)) begin
+						case(colour)
+						(3'b001): ;
+						default: begin 
+							err = 1;
+							$display("Error with  button on");
+							end
+						endcase
+					end
+				end
+			end
+		end
+	end
+			
+	//final test
+	initial begin
+        #1000
+        if (err==0) begin
+          $display("***TEST PASSED! :) ***");
+        $finish;
+        end
+    end
+    
+    
+    
+	//instantiate module
+	lights top (
+	.clk (clk),
+	.rst (rst),
+	.button (button),
+	.colour (colour));
+	
+endmodule
