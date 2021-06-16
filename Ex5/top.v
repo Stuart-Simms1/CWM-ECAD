890ae6cd310b12552d1f5e6dec2909f08a9dbd40
Stuart-Simms1 <stuart.simms@stcatz.ox.ac.uk>
2021-06-16 10:00:48 +0000

    Exercise 5, I think its all tested but I could be missing something

--- Ex5/top.v
+++ Ex5/top.v
@@ -12,4 +12,80 @@
 //  outputs:
 //           heating, cooling
 //////////////////////////////////////////////////////////////////////////////////
-
+`timescale 1ns / 100ps
+module heater (
+	input clk,
+	input [4:0] temperature,
+	output reg heating,
+	output reg cooling);
+	
+	//wires and regs
+	reg [4:0] previous;
+	
+	//function of the module
+	always @ (negedge clk) begin
+		previous <= temperature;
+	end
+	always @ (posedge clk) begin
+		if(temperature >= 5'b10110) begin // some initial values for heating and cooling
+			cooling = 1;
+			heating = 0;
+		end
+		else begin
+			if(temperature <= 5'b10010) begin
+				cooling = 0;
+				heating = 1;
+			end
+			else begin
+				if((heating ==1) && (cooling == 0)) begin
+					if(temperature < 5'b10100) begin
+						heating = 1;
+						cooling = 0;
+					end
+					if(temperature >= 5'b10100) begin
+						heating = 0;
+						cooling = 0;
+					end
+				end
+				else begin
+					if((heating == 0) && (cooling == 0)) begin
+						if(temperature <= 5'b10010) begin
+							heating = 1;
+							cooling = 0;
+						end
+						if((temperature > 5'b10010) && (temperature < 5'b10110)) begin
+							heating = 0;
+							cooling = 0;
+						end
+						if(temperature >= 5'b10110) begin
+							heating = 0;
+							cooling = 1;
+						end
+					end
+					else begin 
+						if((heating == 0) && (cooling == 1)) begin
+							if(temperature <= 5'b10100) begin
+								heating = 0;
+								cooling = 0;
+							end
+							if(temperature > 5'b10100) begin
+								heating = 0;
+								cooling = 1;
+							end
+						end
+						else begin
+							$display("heating and cooling undefined, going idle");	//catchall case since there is a temperature range where if we begin in that range it won't have a defined heating state. I am making it default to idle here basically
+							heating = 0;
+							cooling = 0;
+						end
+					end
+				end
+			end
+		end
+	end
+	
+	
+	
+	
+	
+endmodule
--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -6,4 +6,84 @@
 // Description: A testbench module to test Ex5 - Air Conditioning
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
+module top_tb();
+	//parameters
+	parameter CLK_PERIOD = 10;
+	
+	//wires and regs
+	reg clk;
+	reg [4:0] temperature;
+	wire heating;
+	wire cooling;
+	reg [1:0] state;
+	reg err;
+	
+	
+	//clock generation
+	initial begin	//this sets the clock going from the start
+   		clk = 1'b0;
+    	forever begin
+    	   #(CLK_PERIOD/2);
+    	   clk=~clk;
+    	end
+    end
+    
+	//logic
+	initial begin			//Stores the state of the device
+		forever begin
+		state[1] = heating;
+		state[0] = cooling;
+		#CLK_PERIOD;
+		end
+	end
+	
+	initial begin
+		err = 0;
+		temperature = 5'b11111;		//test for starting in cooling state
+		#CLK_PERIOD;
+		if(state != 2'b01) begin
+			$display("Error with beginning in cooling state");
+			err=1;
+		end
+		
+		temperature = 5'b00000;
+		#CLK_PERIOD;
+		if(state != 2'b10) begin
+			$display("Error with beginning in heating state");
+			err = 1;
+		end
+		
+		temperature = 5'b10100;
+		#CLK_PERIOD;
+		if(state != 2'b00) begin
+			$display("Error with beginning in idle state");
+			err = 1;
+		end
+		
+		forever begin				//I'll just have it cycle temperatures from 0 to 31 and wrap around to check it works
+			#CLK_PERIOD;
+			temperature <= temperature + 5'b1;
+		end
+				
+	end
+	//final test
+	initial begin
+        #100
+        if (err==0) begin
+          $display("***TEST PASSED! :) ***");
+        $finish;
+        end
+    end
+	
+	//instancing the module
+	
+	heater top(
+	.clk (clk),
+	.temperature (temperature),
+	.heating (heating),
+	.cooling (cooling));	
+	
 
+
+endmodule
